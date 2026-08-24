2d4efd9476f0f96a0c1be4b8ced068305a2e0c16
Aidan Strong <aidanmstrong@gmail.com>
2026-08-23 17:39:50 -0700

    apos..

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -15,7 +15,7 @@ function LP() {
       <div className='w-full grid grid-cols-1 sm:grid-cols-7 max-w-4xl text-left text-md md:text-xl text-offWhite font-[Redaction10] h-fit'>
         <div className='sm:col-span-5 sm:col-start-2'>
         <div className='my-10'>
-          <p>If you're in the Los Angeles area and are interested in hosting the cabinet, or want to get in touch, you can reach us at</p>
+          <p>If you&apos;re in the Los Angeles area and are interested in hosting the cabinet, or want to get in touch, you can reach us at</p>
           <p className='pl-5 text-grayboxYellow'>grayboxla[at]gmail[dot]com</p>
         </div>
       </div>
