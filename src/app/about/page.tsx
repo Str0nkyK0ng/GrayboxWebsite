3fbafda9f798b8245b3ef4491f2dad93ed7194d5
Aidan Strong <aidanmstrong@gmail.com>
2026-08-23 17:11:49 -0700

    Fixed about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -15,33 +15,39 @@ function LP() {
       <div className='w-full grid grid-cols-1 sm:grid-cols-7 max-w-4xl text-left text-md md:text-xl text-offWhite font-[Redaction] h-fit'>
         <div className='sm:col-span-5 sm:col-start-2 bg-offBlack'>
         <div className='my-10'>
-          <p>The Graybox Arcade is placed in a new location across Los Angeles every three months. 
+          <p>Graybox Arcade is placed in a new location across Los Angeles every three months. 
             The arcade features work by artists pushing the boundaries of experimental games.</p>
-          <p>Our intention is to release experimental games from the confines of sterile, while-walled galleries, and into the larger arts community.</p>
           <p>We have shown work at:</p>
                  {Projects.map((project)=>{
             return(
-              <p className='pl-5' key={project.workName}><a  className=' text-grayboxYellow hover:underline no-underline' href={project.locationGoogleMapsLink}>{project.venue}</a></p>
+              <p className='pl-5' key={project.workName}><a  className='text-grayboxYellow hover:underline no-underline' href={project.locationGoogleMapsLink}>{project.venue}</a></p>
             )
         })} 
         </div>
 
 
         <div className='my-10'>
-          <p>Our work is constantly inspired by those who came before us, as well as the other contemporaries in our field.</p>
-          <p>Thank you to:</p>
-          {['UCLA Game Lab','Boshi\'s Place', 'LikeLike', 'Hand Eye Society'].map((project)=>{
+          <p>This project is inspired by and in conversation with a variety of spaces for art and games.</p>
+          {
+          [ 'UCLA Game Lab',
+            'Boshi\'s Place',
+            'Killscreen',
+            'Glitch City',
+            'LikeLike',
+            'Hand Eye Society'
+          ]
+          .map((project)=>{
               return(
-                <p key={project}  className='pl-5 text-grayboxYellow ' >{project}</p>
+                <p key={project}  className='pl-5 text-grayboxYellow' >{project}</p>
               )
           })} 
 
         </div>
 
         <div className='my-10'>
-          <p>The Graybox Consists of:</p>
-          <p className='pl-5 text-grayboxYellow'>Vinny Roca</p>
-          <p className='pl-5 text-grayboxYellow'>Aidan Strong</p>
+          <p>Graybox Arcade is run by:</p>
+          <p className='pl-5 text-grayboxYellow'><a href='vinnyroca.info'>Vinny Roca</a></p>
+          <p className='pl-5 text-grayboxYellow'><a href='aidanstrong.info'>Aidan Strong</a></p>
         </div>
 
       
